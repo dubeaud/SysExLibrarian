5fbb426341f5c81a4386e783e252c5b8e7b93aea
Davin Dubeau <davin.dubeau@gmail.com>
2016-09-20 15:37:55 -0400

    added more logging

--- SysExLibrarian/MainWindow.xaml.cs
+++ SysExLibrarian/MainWindow.xaml.cs
@@ -1,37 +1,40 @@
-﻿using LiteDB;
-using System;
-using System.Windows;
-using SysExLibrarian.Models;
-using Sanford.Multimedia.Midi;
-using SysExLibrarian.Properties;
-using Microsoft.Win32;
-using System.IO;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Data;
-using System.Windows.Controls;
-using MaterialDesignThemes.Wpf;
-using Serilog;
-
-namespace SysExLibrarian
+﻿namespace SysExLibrarian
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using LiteDB;
+    using MaterialDesignThemes.Wpf;
+    using Microsoft.Win32;
+    using Sanford.Multimedia.Midi;
+    using Serilog;
+    using SysExLibrarian.Models;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ObservableCollection<SysExFile> LibraryCollection { get; }
         private SysExFile rowBeingEdited = null;
 
         public MainWindow()
         {
             InitializeComponent();
 
-			// Load midi output dropdown with devices
-            for(var i = 0; i < OutputDevice.DeviceCount; i++)
+            Log.Information("Found {deviceCount} MIDI output device(s)", OutputDevice.DeviceCount);
+
+            // Load midi output dropdown with devices
+            for (var i = 0; i < OutputDevice.DeviceCount; i++)
             {
                 MidiOutputComboBox.Items.Add(OutputDevice.GetDeviceCapabilities(i).name);
             }
+
+            Log.Debug("Opening database {DataBaseFileName}", "MyData.db");
 
             using (var db = new LiteDatabase(@"MyData.db"))
             {
@@ -41,51 +44,65 @@ namespace SysExLibrarian
 
                 // Use Linq to query documents
                 var results = files.FindAll();
-                this.LibraryCollection = new ObservableCollection<SysExFile>(results);
-                SysExFilesDataGrid.ItemsSource = this.LibraryCollection;
 
+                var sysExFiles = results as IList<SysExFile> ?? results.ToList();
+                Log.Debug("Found {RowCount} sysex files in database", sysExFiles.Count());
+
+                LibraryCollection = new ObservableCollection<SysExFile>(sysExFiles);
+                SysExFilesDataGrid.ItemsSource = LibraryCollection;
             }
         }
 
+        private ObservableCollection<SysExFile> LibraryCollection { get; }
+
         private void AddFileButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog()
+            Log.Debug("Add file selected");
+
+            var openFileDialog = new OpenFileDialog()
             {
                 Filter = "SysEx files (*.syx)|*.syx",
                 Multiselect = true
             };
-           
-            if (openFileDialog.ShowDialog() == true)
+
+            if (openFileDialog.ShowDialog() != true)
             {
-                foreach (string filename in openFileDialog.FileNames)
+                return;
+            }
+
+            Log.Debug("{SelectedFileCount} files selected" , openFileDialog.FileNames.Length);
+
+            foreach (var filename in openFileDialog.FileNames)
+            {
+                using (var db = new LiteDatabase(@"MyData.db"))
                 {
-                    using (var db = new LiteDatabase(@"MyData.db"))
+                    // Get files collection
+                    var files = db.GetCollection<SysExFile>("SysExFiles");
+
+                    if(files.Exists(f => f.FileName == Path.GetFileName(filename) && f.DiskLocation == filename))
                     {
-                        // Get files collection
-                        var files = db.GetCollection<SysExFile>("SysExFiles");
-
-                        if(files.Exists(f => f.FileName == Path.GetFileName(filename) && f.DiskLocation == filename))
-                        {
-                            MessageBox.Show($"The file \"{Path.GetFileName(filename)}\" already exists in your library and will not be added.", "File Exists", MessageBoxButton.OK, MessageBoxImage.Information);
-                            continue;
-                        }
-
-                        FileInfo fi = new FileInfo(filename);
-                        var sysExFile = new SysExFile()
-                        {
-                            Id = Guid.NewGuid().ToString(),
-                            FileName = Path.GetFileName(filename),
-                            DiskLocation = Path.GetFullPath(filename),
-                            Manufacturer = SysExLibrarian.Helpers.MidiManufacturerHelper.GetMidiManufacturerFromSysExFile(filename),
-                            Size = fi.Length
-                        };
-
-                        files.Insert(sysExFile);
-
-                        this.LibraryCollection.Add(sysExFile);
+                        Log.Warning("File {FileName} already exists in library.", filename);
+                        MessageBox.Show($"The file \"{Path.GetFileName(filename)}\" already exists in your library and will not be added.", "File Exists", MessageBoxButton.OK, MessageBoxImage.Information);
+                        continue;
                     }
-                }      
-            }   
+
+                    var fi = new FileInfo(filename);
+                    var sysExFile = new SysExFile()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        FileName = Path.GetFileName(filename),
+                        DiskLocation = Path.GetFullPath(filename),
+                        Manufacturer = SysExLibrarian.Helpers.MidiManufacturerHelper.GetMidiManufacturerFromSysExFile(filename),
+                        Size = fi.Length
+                    };
+
+                    files.Insert(sysExFile);
+
+                    Log.Debug("Inserted file {@SysExFile}", sysExFile);
+
+                    this.LibraryCollection.Add(sysExFile);
+                }
+            }
         }
 
         private void DeleteFileMenuItem_Click(object sender, RoutedEventArgs e)
@@ -108,7 +125,7 @@ namespace SysExLibrarian
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            App.Current.Shutdown();
+            Application.Current.Shutdown();
         }
 
         private void ShowSysExFileMenuItem_Click(object sender, RoutedEventArgs e)
@@ -121,14 +138,16 @@ namespace SysExLibrarian
 
             if (!File.Exists(selectedItem.DiskLocation))
             {
-                Trace.TraceWarning($"Can not show SysEx file {selectedItem.DiskLocation} because it has been moved or no longer exists.");
+                Log.Warning("Can not show SysEx file {@File} because it has been moved or no longer exists.", selectedItem);
                 return;
             }
 
             // Go to the specified folder and select the file in explorer
-            var runExplorer = new ProcessStartInfo();
-            runExplorer.FileName = "explorer.exe";
-            runExplorer.Arguments = @"/select, " + selectedItem.DiskLocation;
+            var runExplorer = new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                Arguments = @"/select, " + selectedItem.DiskLocation
+            };
             Process.Start(runExplorer);
         }
 
@@ -145,15 +164,7 @@ namespace SysExLibrarian
 
         private async void AboutMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            //About about = new SysExLibrarian.About();
-            //about.ShowDialog();
-            var sampleMessageDialog = new About();
-            //{
-            //    Message = { Text = "Testing 123" }
-            //};
-
-            await DialogHost.Show(sampleMessageDialog, "RootDialog");
-
+            await DialogHost.Show(new About(), "RootDialog");
         }
 
         private void SysExFilesDataGrid_CellEditEnding(object sender, System.Windows.Controls.DataGridCellEditEndingEventArgs e)
@@ -164,32 +175,39 @@ namespace SysExLibrarian
 
         private void SysExFilesDataGrid_CurrentCellChanged(object sender, EventArgs e)
         {
-            if (rowBeingEdited != null)
+            if (rowBeingEdited == null)
             {
-                SysExFile currentFile = null;
-                using (var db = new LiteDatabase(@"MyData.db"))
-                {
-                    // Get files collection
-                    var files = db.GetCollection<SysExFile>("SysExFiles");
-                    currentFile = files.FindOne(f => f.Id == rowBeingEdited.Id);
-                }
+                return;
+            }
 
-                if (!File.Exists(rowBeingEdited.DiskLocation))
-                {
-                    Trace.TraceWarning($"Can not show SysEx file {rowBeingEdited.DiskLocation} because it has been moved or no longer exists.");
-                    return;
-                }
+            SysExFile currentFile = null;
 
-                rowBeingEdited.DiskLocation = rowBeingEdited.DiskLocation.Replace(currentFile?.FileName, rowBeingEdited.FileName);
+            Log.Debug("Opening database {DataBaseFileName}", "MyData.db");
 
-                try
-                {
-                    File.Move(currentFile.DiskLocation, rowBeingEdited.DiskLocation);
-                }
-                catch (Exception ex)
-                {
-                    Trace.TraceError($"An error occurred renaming the file {rowBeingEdited.DiskLocation} Exception: {ex}");
-                }
+            using (var db = new LiteDatabase(@"MyData.db"))
+            {
+                // Get files collection
+                var files = db.GetCollection<SysExFile>("SysExFiles");
+                currentFile = files.FindOne(f => f.Id == rowBeingEdited.Id);
+            }
+
+            if (!File.Exists(rowBeingEdited.DiskLocation))
+            {
+                Log.Warning("Can not show SysEx file {@SysExFile} because it has been moved or no longer exists.", rowBeingEdited);
+                return;
+            }
+
+            rowBeingEdited.DiskLocation = rowBeingEdited.DiskLocation.Replace(currentFile.FileName, rowBeingEdited.FileName);
+
+            Log.Debug("Renaming {@CurrentFile} to {@NewFile}", currentFile, rowBeingEdited);
+
+            try
+            {
+                File.Move(currentFile.DiskLocation, rowBeingEdited.DiskLocation);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred renaming the file {@SysExFile}", rowBeingEdited);
             }
         }
 
